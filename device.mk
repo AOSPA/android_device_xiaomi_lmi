caed53a8196449d0edf0b94dc93659fec3ecf095
Mesquita <mesquita@aospa.co>
2022-08-16 16:26:21 +0000

    lmi: Remove common bt packages
    * They're already being built for all qcom devices after:
      https://gerrit.aospa.co/c/AOSPA/android_device_qcom_common/+/22800
    
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Change-Id: Ib86fc164f0f0bfec357e6ab10ff2adb71a29af86

--- device.mk
+++ device.mk
@@ -71,13 +71,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
-    audio.bluetooth.default \
-    com.dsi.ant@1.0.vendor \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
-    libbluetooth_audio_session \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    libbluetooth_audio_session
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.fflag.override.settings_bluetooth_hearing_aid=true \
